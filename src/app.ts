db8dc281ac0cebcff243524baa359508483398bb
Yuexin Xu <singledogxu@gmail.com>
2024-10-14 22:13:31 -0700

    set up API

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+import * as middlewares from './middlewares';
+import api from './api';
+import MessageResponse from './interfaces/MessageResponse';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+//type of the response
+//MessageResponse: an object that has a message string
+//When a get request comes in to '/' route, we are going to respond with something that is in the format of MessageResponse
+//first argument defines the type of the param, in this case {} means param is empty
+app.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  });
+});
+
+app.use('/api/v1', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+export default app;
